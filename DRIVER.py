b5bcaa64c9a8f616e62ebeca394dab3c76237f30
Steve Westenbroek <westie314@mhtc.net>
2011-12-07 21:30:20 -0600

    Add code snippet to read in Fortran output

--- DRIVER.py
+++ DRIVER.py
@@ -1,8 +1,11 @@
 import numpy as np # import numpy bu call it np
+import re
+from datetime import datetime as dt # pull the datetime module from datetime and call it dt
+import matplotlib.pyplot as plt
 from LLcoolJ import * # import everything from LLcoolJ
 
 # set the namefile
-nf = 'NMOD0.NAM'
+nf = 'NMOD0_new.NAM'
 # set the Basin Area-Lake Area
 DA = 3.7892e8
 # set the initial lake level
@@ -20,6 +23,19 @@ hbud.read_calfile()
 # read in and parse the Data File
 hbud.read_datfile()
 
+file = open('test.out')
+
+fdate = []
+fvalue = []
+datefmt = '%m/%d/%Y' # format to read the dates as
+
+for line in file:
+    # simple regular expression: look for two numerical values
+    # separated by one or more spaces
+    mydate, myvalue =  re.split(r'\s\s+',line.strip())
+    fdate.append(dt.strptime(mydate,datefmt))
+    fvalue.append(myvalue)
+    
 hbud.calc_next_lake_level()
 
 i=-1
